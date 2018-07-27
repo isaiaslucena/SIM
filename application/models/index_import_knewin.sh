5db014c2ff5b3cd57331ee3c838fa3cb456c7df8
isaiasneto <isaiasneto@gmail.com>
2018-07-27 16:00:37 -0300

    committed at 27/07/2018_16:00:03

--- application/models/index_import_knewin.sh
+++ application/models/index_import_knewin.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-php /app/application/models/index_import_knewin_tv.php >> /var/log/index_import_knewin_tv.log
-php /app/application/models/index_import_knewin_radio.php >> /var/log/index_import_knewin_radio.log
+pidtv=$(ps aux| grep "index_import_knewin_tv.php" |grep "php /app/" | awk '{print $2}')
+if [[ -z ${pidtv} ]] ; then
+	php /app/application/models/index_import_knewin_tv.php >> /var/log/index_import_knewin_tv.log &
+# else
+	# echo "Tv script running"
+fi
+
+pidradio=$(ps aux| grep "index_import_knewin_radio.php" |grep "php /app/" | awk '{print $2}')
+if [[ -z ${pidradio} ]] ; then
+	php /app/application/models/index_import_knewin_radio.php >> /var/log/index_import_knewin_radio.log &
+# else
+	# echo "Radio script running"
+fi
