74a5df52091ee8c5e92d5e8e076108a4da11e475
isaiasneto <isaiasneto@gmail.com>
2018-09-27 16:00:14 -0300

    committed at 27/09/2018_16:00:01

--- application/models/index_transc.sh
+++ application/models/index_transc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+now=$(date '+%Y-%m-%d %H:%M:%S')
 scpid=$(ps -C php -f | grep index_transc.php | awk '{print $2}')
 if [[ -z ${scpid} ]] ; then
-	echo 'Script not running!'
+	echo ${now} '- Script not running!'
 	php /app/application/models/index_transc.php >> /var/log/index_transc.log
 else
-	echo 'Script already running...'
+	echo ${now} '- Script already running...'
 fi
